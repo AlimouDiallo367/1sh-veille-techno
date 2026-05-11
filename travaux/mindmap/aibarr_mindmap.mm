a8e8ba09766a8701c8e35df871dac4c337ea12ee
Thierno Alimou Diallo <thalimoud66@gmail.com>
2026-05-10 22:37:42 -0400

    aibarr mindmap added

---
+++ travaux/mindmap/aibarr_mindmap.mm
@@ -0,0 +1,131 @@
+<map version="freeplane 1.12.15">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks/>
+<node TEXT="AiBarr Mindmap" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1778462340691"><hook NAME="MapStyle" background="#2e3440ff" zoom="0.6830134">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/dark_nord_template.mm" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_671184412" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#eceff4" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#88c0d0" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_671184412" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" BORDER_WIDTH="1.9 px">
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ebcb8b">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#484747">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#5e81ac"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_779275544" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_779275544"/>
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#eceff4">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="1. Objectifis et Vision" POSITION="top_or_left" ID="ID_1296870413" CREATED="1778462379970" MODIFIED="1778462589549">
+<node TEXT="Souveraineté des données&#xa;(Local-First)" ID="ID_245217136" CREATED="1778462645726" MODIFIED="1778465814690"/>
+<node TEXT="Indépendance technologique&#xa;(No-Cloud)" ID="ID_296138009" CREATED="1778462674269" MODIFIED="1778465830454"/>
+<node TEXT="Minimalisme et performance (inférence locale)" ID="ID_357106155" CREATED="1778462702020" MODIFIED="1778462716305"/>
+</node>
+<node TEXT="2. Stack technique" POSITION="bottom_or_right" ID="ID_219445337" CREATED="1778462432856" MODIFIED="1778462594266">
+<node TEXT="Laptop perso (Fedora dualboot)" ID="ID_1544096021" CREATED="1778462594268" MODIFIED="1778462786826">
+<node TEXT="CPU: AMD Ryzen 7 7840HS 3.80GHz" ID="ID_1892045935" CREATED="1778462788460" MODIFIED="1778464710139"/>
+<node TEXT="RAM: 32 GB" ID="ID_1838109852" CREATED="1778462798993" MODIFIED="1778464721108"/>
+<node TEXT="GPU: GeForce RTX 4060 Laptop 8GB" ID="ID_682737661" CREATED="1778462808246" MODIFIED="1778465868679"/>
+</node>
+<node TEXT="Logiciel" ID="ID_1906500558" CREATED="1778462752397" MODIFIED="1778462823344">
+<node TEXT="Moteur: Ollama" ID="ID_857518335" CREATED="1778462823345" MODIFIED="1778462838171"/>
+<node TEXT="Backend: Llama.cpp (quantification)" ID="ID_1888721793" CREATED="1778462841833" MODIFIED="1778462859590"/>
+<node TEXT="Interface: Open WebUI" ID="ID_1411504555" CREATED="1778462860894" MODIFIED="1778463445506"/>
+<node TEXT="Conteneurisation: Incus/LXC ou Docker" ID="ID_1829527403" CREATED="1778463453242" MODIFIED="1778463519414"/>
+</node>
+</node>
+<node TEXT="3. Modèles de langage (LLMs)" POSITION="bottom_or_right" ID="ID_128644188" CREATED="1778462498102" MODIFIED="1778462604628">
+<node TEXT="Généralistes: Llama 3, Gemma 4, Mistral" ID="ID_985369579" CREATED="1778463583280" MODIFIED="1778463603376"/>
+<node TEXT="Spécialiste: DeepSeek-Coder" ID="ID_1187624884" CREATED="1778463708291" MODIFIED="1778463732850"/>
+</node>
+<node TEXT="4. Cas d&apos;Usage et implémentation" POSITION="top_or_left" ID="ID_1560955863" CREATED="1778462526402" MODIFIED="1778462632644">
+<node TEXT="RAG (Priorité MVP)" ID="ID_1761514588" CREATED="1778464487576" MODIFIED="1778464593475">
+<node TEXT="Indexation de documents (.md, .pdf)" ID="ID_1262513804" CREATED="1778465439924" MODIFIED="1778465457953"/>
+<node TEXT="Procédures GLPI/OneNote/Omnivox (Cégep)" ID="ID_697320449" CREATED="1778465461695" MODIFIED="1778465501509"/>
+<node TEXT="Configs Cisco/Fortigate" ID="ID_1703784285" CREATED="1778465478797" MODIFIED="1778465492115"/>
+<node TEXT="Pas de Fine-Tuning (Flexibilité)&#xa;[L&apos;IA consomme mon contenu]" ID="ID_1652658222" CREATED="1778465523927" MODIFIED="1778465556161"/>
+<node TEXT="Contenu web (nbourre, jhoffman, rust docs)&#xa;[Défi]" ID="ID_1210192830" CREATED="1778466013475" MODIFIED="1778466047225"/>
+</node>
+<node TEXT="ShawAI" ID="ID_1586542675" CREATED="1778464600052" MODIFIED="1778464612393">
+<node TEXT="Automatisation, scripting&#xa;(PowerShell, Bash, Ruby, AD)" ID="ID_1669741209" CREATED="1778465569899" MODIFIED="1778465611769"/>
+</node>
+<node TEXT="R&amp;D Sandbox&#xa;(Perso)" ID="ID_426719917" CREATED="1778464613978" MODIFIED="1778465643047">
+<node TEXT="Workflow Neovim (Lua), optimisation et projets futures" ID="ID_29565763" CREATED="1778465644852" MODIFIED="1778465699524"/>
+</node>
+<node TEXT="Sécurité (SecBarr-CLI)" ID="ID_608475926" CREATED="1778464627703" MODIFIED="1778465730652">
+<node TEXT="Audit réseau Air-Gapped (Rust)" ID="ID_345544443" CREATED="1778465708118" MODIFIED="1778465721116"/>
+</node>
+</node>
+<node TEXT="5. Veille et concepts avancés" POSITION="bottom_or_right" ID="ID_131757527" CREATED="1778462529936" MODIFIED="1778462640644">
+<node TEXT="MCP (Model Context Protocol)" ID="ID_1454237183" CREATED="1778463928002" MODIFIED="1778463969528"/>
+<node TEXT="RAG avancée (Context window, Embeddings, Vector DB/Chroma DB)&#xa;[si le temps me permet, sinon intégration après cours]" ID="ID_125283189" CREATED="1778463945369" MODIFIED="1778466816568"/>
+</node>
+</node>
+</map>
